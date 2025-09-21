6800f6df113e35530aa0de0e83f4dbfd686bfdc7
Dayane Bravo <fiasiii@gmail.com>
2025-09-21 15:55:16 -0300

    aula 02

---
+++ aula_02/aula_02_pratica.R
@@ -0,0 +1,32 @@
+################ TEMA 1 – MEDIDAS DE TENDÊNCIA CENTRAL ########################
+
+### MEDIA
+idades <- c(25, 30, 35, 40, 45)  
+media <- mean(idades)  
+print(media)
+
+
+idades_outlier <- c(25, 30, 35, 40, 45, 100)  
+media_outlier <- mean(idades_outlier)  
+print(media_outlier)  
+
+
+
+
+
+### MEDIANA
+idades <- c(25, 30, 35, 40, 45)  
+mediana <- median(idades)  
+print(mediana) 
+
+
+idades_outlier <- c(25, 30, 35, 40, 45, 100)  
+mediana_outlier <- median(idades_outlier)  
+print(mediana_outlier)  
+
+
+
+
+
+
+
